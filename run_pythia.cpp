6ed03b2e5862ddffd686e9ac0350b654394aacc1
la1993628 <57288284+la1993628@users.noreply.github.com>
2023-08-14 11:42:35 +0800

    Update run_pythia.cpp

--- run_pythia.cpp
+++ run_pythia.cpp
@@ -9,14 +9,14 @@ int main() {
     auto start = std::chrono::high_resolution_clock::now();
 
     Pythia8::Pythia pythia;
-    pythia.readString("Beams:eCM = 5020."); // Set initial energy to 5020GeV
+    pythia.readString("Beams:eCM = 200."); // Set initial energy to 5020GeV
     pythia.readString("HardQCD:all = on");
-    pythia.readString("PhaseSpace:pTHatMin = 20");
+    pythia.readString("PhaseSpace:pTHatMin = 5");
     pythia.readString("Print:quiet = on"); // Disable standard output
     pythia.init();
 
-    int nEvents = 10000;
-    std::ofstream outFile("parton.dat");
+    int nEvents = 100000;
+    std::ofstream outFile("parton_trigger_5.dat");
 
     for (int iEvent = 0; iEvent < nEvents; ++iEvent) {
         if (!pythia.next()) continue;
@@ -25,17 +25,20 @@ int main() {
 
         int particleCount = 0; // Count of final state parton-level particles
 
-        outFile << "#" << iEvent + 1 << "\n";
+        outFile << "#" << "\n";
 
         for (int i = 0; i < event.size(); ++i) {
-            if (event[i].isFinalPartonLevel()) {
+            if (event[i].isFinalPartonLevel() && 
+                (abs(event[i].id()) == 1 || abs(event[i].id()) == 2 || abs(event[i].id()) == 3 || abs(event[i].id()) == 21) 
+               )
+            {
                 outFile << particleCount + 1 << " " << event[i].id() << " " << event[i].name() << " "
                         << event[i].px() << " " << event[i].py() << " " << event[i].pz() << " " << event[i].e() << "\n";
                 ++particleCount;
             }
         }
 
-        outFile << "#" << iEvent + 1 << " " << particleCount << " end\n";
+        outFile << "#"  << "\n";
     }
 
     outFile.close();
@@ -50,6 +53,7 @@ int main() {
     std::time_t start_time_t = std::chrono::system_clock::to_time_t(start);
     std::time_t end_time_t = std::chrono::system_clock::to_time_t(end);
 
+    std::cout << "nEvents: " << nEvents << endl;
     std::cout << "Start time: " << std::ctime(&start_time_t);
     std::cout << "End time: " << std::ctime(&end_time_t);
     std::cout << "Total time elapsed: " << duration.count() << " seconds" << std::endl;
